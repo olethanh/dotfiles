97959aff1878539a3a4f2d65d7ea6d696985a98b
Olivier Le Thanh Duong <olivier@lethanh.be>
2015-12-01 13:08:34 +0100

    add fallck on install.py was causing error on lxc, don't know why yet

--- install.py
+++ install.py
@@ -7,9 +7,14 @@ for filename in os.listdir("dot"):
     src = os.path.realpath("dot/" + filename)
     if os.path.exists(target):
         print "target", target, "already exists"
+    elif not os.path.exists(src):
+        print "Error src doen't exist:", src
     else:
         print "Creating target:", target, " from source: ", src
-        os.symlink(src, target)
+        try:
+            os.symlink(src, target)
+        except OSError as e:
+            print e, target, src
 
 # Install vundle if not existant
 if not  os.path.exists(os.path.expanduser("~/.vim/bundle/vundle/")):
