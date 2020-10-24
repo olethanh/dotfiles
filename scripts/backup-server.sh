e636f00ad1e193d498c8815f213ca075ba5381aa
Olivier Le Thanh Duong <olivier@lethanh.be>
2020-10-24 22:12:41 +0200

    Add custom bash script
    let's try this instead of functions

---
+++ scripts/backup-server.sh
@@ -0,0 +1,3 @@
+#/bin/zsh
+#set -eu
+rsync -r olivier@agot.be:backups/ $HOME/backup/server-ovh/
---
+++ scripts/home-both-displays.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1-3-1 --auto --left-of DP-1-3-2 --output DP-1-3-2 --mode "1920x1080"
